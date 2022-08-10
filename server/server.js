aab885304367e315127edd047e131e2efccf7a65
mdhossin <mdsahadathossin778@gmail.com>
2022-08-10 14:17:41 +0600

    successfully deployed on heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
@@ -24,6 +24,15 @@ app.use(cookieParser());
 // app.use("/api/v1", require("./src/v1/routes"));
 
 app.use("/api/auth", userRoutes);
+
+// delpoy code
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
